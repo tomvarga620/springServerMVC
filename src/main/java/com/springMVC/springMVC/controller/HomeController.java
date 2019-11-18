dccb6bc71795cbf849952813f10a2e634fd3ea3f
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-11-18 18:38:44 +0100

    initial commit

---
+++ src/main/java/com/springMVC/springMVC/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springMVC.springMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
