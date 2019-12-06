9455ccde0f9d6db9fc36d8e40001681538d8daf3
tomvarga620 <tomas.varga@akademiasovy.sk>
2019-12-06 12:12:09 +0100

    135

--- src/main/java/com/springMVC/springMVC/Student.java
+++ src/main/java/com/springMVC/springMVC/Student.java
@@ -1,12 +1,26 @@
 package com.springMVC.springMVC;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
 
-    public Student(){
+    private String country;
 
+    private String favoriteLanguage;
+
+    private LinkedHashMap<String, String> countryOptions;
+
+    public Student(){
+        countryOptions = new LinkedHashMap<>();
+
+        countryOptions.put("BR","Brazil");
+        countryOptions.put("FR","France");
+        countryOptions.put("DE","Germany");
+        countryOptions.put("IN","India");
+        countryOptions.put("USA","United States of America");
     }
 
     public String getFirstName() {
@@ -23,5 +37,29 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public LinkedHashMap<String, String> getCountryOptions() {
+        return countryOptions;
+    }
+
+    public void setCountryOptions(LinkedHashMap<String, String> countryOptions) {
+        this.countryOptions = countryOptions;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 }
--- src/main/webapp/WEB-INF/pages/student-confirmation.jsp
+++ src/main/webapp/WEB-INF/pages/student-confirmation.jsp
@@ -10,6 +10,14 @@
 
 The student is confirmed: ${student.firstName} ${student.lastName}
 
+<br><br>
+
+Country: ${student.country}
+
+<br><br>
+
+Favorite Language: ${student.favoriteLanguage}
+
 </body>
 
 </html>
